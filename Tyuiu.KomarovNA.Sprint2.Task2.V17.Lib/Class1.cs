c54965ad748cbcaead2d40b591ed2a3e957a16d6
gravity34031 <gravity2507@gmail.com>
2024-09-21 14:24:35 +0500

    app-lib-test creation

---
+++ Tyuiu.KomarovNA.Sprint2.Task2.V17.Lib/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Tyuiu.KomarovNA.Sprint2.Task2.V17.Lib
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ Tyuiu.KomarovNA.Sprint2.Task2.V17.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Tyuiu.KomarovNA.Sprint2.Task2.V17.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
