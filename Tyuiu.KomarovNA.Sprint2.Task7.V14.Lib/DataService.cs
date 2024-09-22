7767fb9b1190c082ede0d12e9e55c424c5814b30
gravity34031 <gravity2507@gmail.com>
2024-09-22 22:49:45 +0500

    task7

---
+++ Tyuiu.KomarovNA.Sprint2.Task7.V14.Lib/DataService.cs
@@ -0,0 +1,25 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.KomarovNA.Sprint2.Task7.V14.Lib
+{
+    public class DataService : ISprint2Task7V14
+    {
+        public bool CheckDotInShadedArea(double x, double y)
+        {
+            bool insideCircle = x * x + y * y <= 1;
+
+            // Проверка на нахождение выше y = x и ниже y = -x
+            bool withinBounds = y >= x && y <= -x;
+
+            // Проверка на нахождение в заштрихованной области
+            if (insideCircle && withinBounds)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Tyuiu.KomarovNA.Sprint2.Task7.V14.Test/DataServiceTest.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.KomarovNA.Sprint2.Task7.V14.Test
+{
+    public class DataServiceTest
+    {
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
