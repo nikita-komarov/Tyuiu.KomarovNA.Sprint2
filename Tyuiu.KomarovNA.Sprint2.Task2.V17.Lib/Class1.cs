28341ded8be7855c5a2faa223c7439a2b71b9da8
gravity34031 <gravity2507@gmail.com>
2024-09-22 14:36:00 +0500

    task2 test ru symbols

---
+++ Tyuiu.KomarovNA.Sprint2.Task2.V17.Lib/Class1.cs
@@ -0,0 +1,14 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.KomarovNA.Sprint2.Task1.V17.Lib
+{
+    public class DataService : ISprint2Task2V17
+    {
+        public bool CheckDotInShadedArea(int x, int y)
+        {
+            Console.WriteLine("РУсский текст");
+            // немного ру
+            return true;
+        }
+    }
+}
---
+++ Tyuiu.KomarovNA.Sprint2.Task2.V17.Test/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace Tyuiu.KomarovNA.Sprint2.Task2.V17.Test;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
