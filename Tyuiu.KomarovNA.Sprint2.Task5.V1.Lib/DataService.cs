c9fcb04546d952fd7227b89f518944fde03197bb
gravity34031 <gravity2507@gmail.com>
2024-09-22 22:24:46 +0500

    task5

---
+++ Tyuiu.KomarovNA.Sprint2.Task5.V1.Lib/DataService.cs
@@ -0,0 +1,38 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.KomarovNA.Sprint2.Task5.V1.Lib
+{
+    public class DataService : ISprint2Task5V1
+    {
+        public int FindMonthDaysCount(int value)
+        {
+            int days;
+
+            switch (value)
+            {
+                case 1: // Январь
+                case 3: // Март
+                case 5: // Май
+                case 7: // Июль
+                case 8: // Август
+                case 10: // Октябрь
+                case 12: // Декабрь
+                    days = 31;
+                    break;
+                case 4: // Апрель
+                case 6: // Июнь
+                case 9: // Сентябрь
+                case 11: // Ноябрь
+                    days = 30;
+                    break;
+                case 2: // Февраль
+                    days = 28;
+                    break;
+                default:
+                    Console.WriteLine("Некорректный номер месяца.");
+                    return 0;
+            }
+            return days;
+        }
+    }
+}
---
+++ Tyuiu.KomarovNA.Sprint2.Task5.V1/Program.cs
@@ -0,0 +1,43 @@
+﻿using Tyuiu.KomarovNA.Sprint2.Task5.V1.Lib;
+namespace Tyuiu.KomarovNA.Sprint2.Task5.V1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #2 | Выполнил: Комаров Никита Алексеевич | ИИПБ-24-1";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Оператор if - полная и короткая форма записи                            *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант #1                                                            *");
+            Console.WriteLine("* Выполнил: Комаров Никита Алексеевич | ИИПБ-24-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *"); 
+            
+            Console.WriteLine("*Написать программу на, которая запрашивает целые значения с клавиатуры и *");
+            Console.WriteLine("* вычисляет находится ли точка с координатами X,Y в заштрихованной области*");
+            Console.WriteLine("*и.                                                                       *");
+
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            var result = ds.YOURFUNCTION();
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
