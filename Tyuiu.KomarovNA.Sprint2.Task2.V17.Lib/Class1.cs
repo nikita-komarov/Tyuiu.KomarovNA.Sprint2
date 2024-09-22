6553c01b389e4a34f097f736a7791c4c00a6f08a
gravity34031 <gravity2507@gmail.com>
2024-09-22 14:25:51 +0500

    task2-autogenerate-test

--- Tyuiu.KomarovNA.Sprint2.Task2.V17.Lib/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Tyuiu.KomarovNA.Sprint2.Task2.V17.Lib
-{
-    public class Class1
-    {
-
-    }
-}
--- Tyuiu.KomarovNA.Sprint2.Task2.V17.Test/DataServiceTest.cs
+++ Tyuiu.KomarovNA.Sprint2.Task2.V17.Test/DataServiceTest.cs
@@ -1,11 +1,10 @@
 namespace Tyuiu.KomarovNA.Sprint2.Task2.V17.Test
 {
-    [TestClass]
-    public class UnitTest1
+    public class DataServiceTest
     {
-        [TestMethod]
         public void TestMethod1()
         {
+            Assert.AreEqual(1, 1);
         }
     }
 }
